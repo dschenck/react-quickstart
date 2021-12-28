5053788924ec0adc7da240394b313d1e2a101dcf
David Schenck <david.schenck@outlook.com>
2021-12-28 17:58:02 +0100

    hoist actions to module level

--- src/modules/prompts/index.js
+++ src/modules/prompts/index.js
@@ -1,9 +1,10 @@
-import actions   from './actions'
+import actions from './actions'
 import Component from './components'
 import constants from './constants'
 
-export default { 
-    actions, 
+export default {
+    actions,
+    ...actions,
     Component,
     ...constants
 }
